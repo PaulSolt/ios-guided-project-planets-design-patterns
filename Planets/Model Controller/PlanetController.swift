c4cc3d8ade69c73361d8bb32fe4efcb75eafac28
Paul Solt <PaulSolt@gmail.com>
2019-08-22 12:41:39 -0400

    Added the Facade pattern to clean up the interaction between the planetController and the collection view.

--- Planets/Model Controller/PlanetController.swift
+++ Planets/Model Controller/PlanetController.swift
@@ -8,27 +8,25 @@
 
 import Foundation
 
+// Facade Pattern
+
 class PlanetController {
-    var planetsWithoutPluto = [
-        Planet(name: "Mercury", imageName: "mercury"),
-        Planet(name: "Venus", imageName: "venus"),
-        Planet(name: "Earth", imageName: "earth"),
-        Planet(name: "Mars", imageName: "mars"),
-        Planet(name: "Jupiter", imageName: "jupiter"),
-        Planet(name: "Saturn", imageName: "saturn"),
-        Planet(name: "Uranus", imageName: "uranus"),
-        Planet(name: "Neptune", imageName: "neptune"),
-        ]
     
-    var planetsWithPluto = [
-        Planet(name: "Mercury", imageName: "mercury"),
-        Planet(name: "Venus", imageName: "venus"),
-        Planet(name: "Earth", imageName: "earth"),
-        Planet(name: "Mars", imageName: "mars"),
-        Planet(name: "Jupiter", imageName: "jupiter"),
-        Planet(name: "Saturn", imageName: "saturn"),
-        Planet(name: "Uranus", imageName: "uranus"),
-        Planet(name: "Neptune", imageName: "neptune"),
-        Planet(name: "Pluto", imageName: "pluto"),
+    var planets: [Planet] {
+        var planetArray = [
+            Planet(name: "Mercury", imageName: "mercury"),
+            Planet(name: "Venus", imageName: "venus"),
+            Planet(name: "Earth", imageName: "earth"),
+            Planet(name: "Mars", imageName: "mars"),
+            Planet(name: "Jupiter", imageName: "jupiter"),
+            Planet(name: "Saturn", imageName: "saturn"),
+            Planet(name: "Uranus", imageName: "uranus"),
+            Planet(name: "Neptune", imageName: "neptune"),
         ]
+        
+        if UserDefaults.standard.bool(forKey: .shouldShowPlutoKey) {
+            planetArray.append(Planet(name: "Pluto", imageName: "pluto"))
+        }
+        return planetArray
+    }
 }
