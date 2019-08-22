414ecc0ab5f80e300d2722c98154fb9e587a95ac
Paul Solt <PaulSolt@gmail.com>
2019-08-22 14:22:07 -0400

    Added state restoration.

--- Planets/AppDelegate.swift
+++ Planets/AppDelegate.swift
@@ -16,6 +16,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first)
+        
+        return true
+    }
+    
+    
+    // Enable state restoration (Momento Design Pattern)
+    
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        
+        // Can have some cases where you don't want this to save/restore
+//        if Users.shared.userIsLoggingIn {
+//            return false
+//        }
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
         return true
     }
 
--- Planets/View Controllers/PlanetDetailViewController.swift
+++ Planets/View Controllers/PlanetDetailViewController.swift
@@ -27,6 +27,29 @@ class PlanetDetailViewController: UIViewController {
         label.text = planet.name
     }
     
+    // MARK: State Restoration
+    
+    override func encodeRestorableState(with coder: NSCoder) {
+        super.encodeRestorableState(with: coder)
+        // save state
+        
+        // Planet -> Data -> Encode
+        
+        guard let planet = planet else { return }
+        let planetData = try? PropertyListEncoder().encode(planet)
+        coder.encode(planetData, forKey: "planetData")
+    }
+    
+    override func decodeRestorableState(with coder: NSCoder) {
+        super.decodeRestorableState(with: coder)
+        // load state
+        
+        // Data -> Planet -> Update the View
+        guard let planetData = coder.decodeObject(forKey: "planetData") as? Data else { return }
+        planet = try? PropertyListDecoder().decode(Planet.self, from: planetData)
+    }
+    
+    
     var planet: Planet? {
         didSet {
             updateViews()
